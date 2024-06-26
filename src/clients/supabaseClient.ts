71450e7d134e758c7440bad7e43af14fce5705cc
Relectrik <viv@dluvs.com>
2024-06-25 17:54:53 -0700

    readded NEXT_PUBLIC so production works

--- src/clients/supabaseClient.ts
+++ src/clients/supabaseClient.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 if (!process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL) throw new Error('NEXT_PUBLIC_SUPABASE_PROJECT_URL is not set')
-if (!process.env.SUPABASE_ANON_API_KEY) throw new Error('SUPABASE_ANON_API_KEY is not set')
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_API_KEY) throw new Error('NEXT_PUBLIC_SUPABASE_ANON_API_KEY is not set')
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_API_KEY
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_API_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
