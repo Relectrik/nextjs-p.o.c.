6a9f37815c5bbd28824a9fdb9f5518eee98441e6
Relectrik <viv@dluvs.com>
2024-07-31 02:14:15 +0800

    create new URL object to test deployment

--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -15,7 +15,7 @@ export async function GET() {
       access_type: 'online',
       scope: scopes,
     })
-    return NextResponse.redirect(url)
+    return NextResponse.redirect(new URL(url))
   } catch (error) {
     console.error('Error fetching Google Sheets data:', error)
     return new NextResponse(JSON.stringify(error), { status: 400 })
